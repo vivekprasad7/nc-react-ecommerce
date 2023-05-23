d4bbfc775a42649c6d55a5d5bbe49118b3e54d39
Vivek Prasad <flashtech007@gmail.com>
2023-05-23 20:48:24 +0530

    feat - added routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Mockman from "mockman-js";
 import { Home } from "./pages/home/home";
+import { ProductListing } from "./pages/productListing/productListing";
+import { ProductDetails } from "./pages/productDetails/productDetails";
+import { Wishlist } from "./pages/wishlist/wishlist";
+import { Cart } from "./pages/cart/cart";
 
 
 function App() {
@@ -9,15 +13,20 @@ function App() {
     <div className="App">
       <nav>
         <NavLink to="/">Home</NavLink>  {" "} || {" "}
-        <NavLink to="/mockman">Mockman</NavLink> 
-
-
-
+        <NavLink to="/mockman">Mockman</NavLink> {" "} || {" "}
+        <NavLink to="/products">Products</NavLink>  {" "} || {" "}
+        <NavLink to="/cart">Cart</NavLink>  {" "} || {" "}
+        <NavLink to="/wishlist">Wishlist</NavLink>  {" "} || {" "}
+        <NavLink to="/">Home</NavLink>  {" "} || {" "}   
       </nav>
 
       <Routes>
       <Route path="/" element={<Home/>}/>
-        <Route path="/mockman" element={<Mockman/>}/>
+      <Route path="/products" element={<ProductListing/>}/>
+      <Route path="/products/:productID" element={<ProductDetails/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/wishlist" element={<Wishlist/>}/>
+      <Route path="/mockman" element={<Mockman/>}/>
       </Routes>
       
     </div>
---
+++ src/pages/productListing/productListing.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ProductListing = () => {
+  return (
+    <div>ProductListing</div>
+  )
+}
