c835ac8514c6ce4dda09b027d3c85bdd6894930c
Vivek Prasad <flashtech007@gmail.com>
2023-05-29 14:43:37 +0530

    updated empty cart page

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Cart = () => {
+  const navigate = useNavigate();
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <h2>Cart</h2>
+      <div className='cart-items'>
+        <h3>Your cart is empty!</h3>
+        <p>Looks like your cart is sad. Make it happy by adding some delicacies.</p>
+        <button onClick={() => navigate('/products')}>Add Items</button>
+      </div>
+
+    </div>
   )
 }
