b3c7b535c26c03ca88653103247a9860ee8d51f3
Vivek Prasad <flashtech007@gmail.com>
2023-05-30 18:34:40 +0530

    style - customized cart and wishlist

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import"./cart.css"
+import { useDataContext } from '../../contexts/dataContext';
 
 export const Cart = () => {
   const navigate = useNavigate();
+
+  const {dataState:{cart}} = useDataContext()
 
   const getData = async () => {
     try{
@@ -38,12 +41,68 @@ export const Cart = () => {
 } 
   return (
     <div className='cart'>
-      <h2 onClick={getData}>Cart</h2>
-      <div className='cart-items'>
+      {/* <h2 onClick={getData}>Cart</h2> */}
+
+      <section className='cart-items'>
+        {
+          cart?.length === 0 &&<div className='empty-cart-items'>
         <h3>Your cart is empty!</h3>
         <p>Looks like your cart is sad. Make it happy by adding some delicacies.</p>
         <button onClick={() => navigate('/products')}>Add Items</button>
       </div>
+        }
+
+        {
+          cart?.map((item) => {
+            
+
+            return(
+              <div className="cart-product">
+                <div className='img'>
+                <button className='cart-heart'><i className='fa fa-heart'></i></button>
+
+                  <img src={item.image} alt={item.title}  />
+
+                  <div className='cart-details'>
+                  <h4>{item.title}</h4>
+                  <h4>${item.price}.00 * {item.qty} 
+                  <span></span></h4>
+
+                </div>
+
+                 
+                </div>
+                
+               
+
+                <div className='cart-inc-dec'>
+
+                    <button><i className='fa fa-plus'></i></button>
+                    <div>0</div>
+                    <button><i className='fa fa-minus'></i></button>
+                    <button><i className='fa fa-trash'></i></button>
+
+
+                </div>
+
+              </div>
+            )
+          })
+        }
+
+      </section>
+
+      <aside className='cart-total'>
+        <h2>Cart Summary</h2>
+        <div>
+          <h3>Total Price:</h3>
+          <h3>100$</h3>
+        </div>
+      </aside>
+
+
+
+      
 
     </div>
   )
--- src/pages/wishlist/wishlist.jsx
+++ src/pages/wishlist/wishlist.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useDataContext } from '../../contexts/dataContext'
 import { ProductCard } from '../../components/product-card/productCard'
+import "./wishlist.css"
 
 export const Wishlist = () => {
   const {dataState:{wishlist}} = useDataContext()
   return (
     <div>
-      <div>
+      <div className='wishlist-container'>
         {
           wishlist.map((item) => {
 
