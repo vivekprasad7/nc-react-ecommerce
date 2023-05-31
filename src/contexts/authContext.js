99bfb505646bb1d75f05ff42bdb914392a8ba35b
Vivek Prasad <flashtech007@gmail.com>
2023-06-01 04:24:04 +0530

    updated auth context and reducer

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,79 +1,88 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
 import { authReducer } from "../reducers/authReducer";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({children}) => {
 
+    const [loginInput, setLoginInput] = useState({email:"", password:""})
 
-    const authInitial ={
+    const [signupInput, setSignupInput] = useState({name:"", email:"",password:""})
+
+    const authInitial = {
+        authLoading: false,
         isLoggedIn: false,
-        user:{}, 
-        token:'',
+        user:{},
+        token:"",
+        error:"",
     }
 
+    const [authState, authDispatch] = useReducer(authReducer, authInitial);
 
-    const [ authState, authDispatch] = useReducer(authReducer, authInitial)
     const navigate = useNavigate();
+    const location = useLocation();
 
 
-    const userLogin = async (loginData) => {
+    const loginHandler = async (loginInput) => {
         try{
+            authDispatch({type: "SET_AUTH_LOADING", payload:true})
             const {data, status} = await axios({
                 method:"POST",
-                data:loginData,
-                url:"/api/auth/login"
-            })
-
-            console.log("authcontext",data);
-            if(status ===200){
-                authDispatch({type:"SET_ISLOGGEDIN", payload:true})
-                authDispatch({type:"SET_USER", payload:data?.foundUser})
-                authDispatch({type:"SET_LOGGEDIN", payload:data?.encodedToken})
-                navigate('/');
-                localStorage.setItem('token', data?.encodedToken)
+                data:loginInput,
+                url:"api/auth/login"
+            });
+            if (status === 200){
+                authDispatch({type: "SET_ISLOGGEDIN", payload:true})
+                authDispatch({type: "SET_USER", payload:data?.foundUser})
+                authDispatch({type: "SET_TOKEN", payload:data?.encodedToken})
+                authDispatch({type: "SET_AUTH_LOADING", payload:false})
+                navigate(location?.state?.from?.pathname || "/")
+                localStorage.setItem("token", data?.encodedToken)
             }
-
-        
         } catch(e){
-            console.error(e)
+            console.error(e);
+            if(e.request.status === 401){
+                authDispatch({type: "SET_ERROR", payload: "WRONG PASSWORD"})
+            } else{
+                authDispatch({type: "SET_ERROR", payload: "USER NOT FOUND"})
+            }
         }
     }
 
-    const userSignup = async (signupData) => {
+    const signupHandler = async (signupInput) => {
         try{
-            const {data, status} = axios ({
+            authDispatch({type: "SET_AUTH_LOADING", payload:true})
+            const {data, status} = await axios({
                 method:"POST",
-                data:signupData,
-                url:'/api/auth/signup'
-            })
-            if(status === 201){
-                authDispatch({type:"SET_ISLOGGEDIN", payload:true})
-                authDispatch({type:"SET_USER", payload:data?.createdUser})
-                authDispatch({type:"SET_TOKEN", payload:data?.encodedToken})
-                navigate('/');
-                localStorage.setItem('token', data?.encodedToken)
+                data:signupInput,
+                url:"api/auth/signup"
+            });
+            if (status === 201){
+                authDispatch({type: "SET_ISLOGGEDIN", payload:true})
+                authDispatch({type: "SET_USER", payload:data?.createdUser})
+                authDispatch({type: "SET_TOKEN", payload:data?.encodedToken})
+                authDispatch({type: "SET_AUTH_LOADING", payload:false})
+                navigate(location?.state?.from?.pathname || "/")
+                localStorage.setItem("token", data?.encodedToken)
             }
-
         } catch(e){
-            console.error(e)
+            console.error(e);
+           
         }
     }
 
-    const userLogout = () => {
-        authDispatch({type:"SET_ISLOGGEDIN", payload:false})
-        authDispatch({type:"SET_USER", payload:{}})
-        authDispatch({type:"SET_TOKEN", payload:''})
-        localStorage.setItem('token', '')
+    const logoutHandler = () => {
+        authDispatch({type: "SET_ISLOGGEDIN", payload:false})
+        authDispatch({type: "SET_USER", payload:{}})
+        authDispatch({type: "SET_TOKEN", payload:""})
+        localStorage.setItem("token", "")
     }
-
-
 
     return(
         <>
-        <AuthContext.Provider value={{ authState, userLogin, userSignup, userLogout}}>
+        <AuthContext.Provider value={{ authState, loginHandler, signupHandler, logoutHandler, loginInput, setLoginInput, signupInput, setSignupInput}}>
             {children}
         </AuthContext.Provider>
         </>
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,11 +1,15 @@
 export const authReducer = (state, action) => {
     switch(action.type){
+        case"SET_AUTH_LOADING":
+        return{...state, authLoading : action.payload}
         case"SET_ISLOGGEDIN":
         return{...state, isLoggedIn : action.payload}
         case"SET_USER":
         return{...state, user: action.payload}
         case"SET_TOKEN":
         return{...state, token: action.payload}
+        case"SET_ERROR":
+        return{...state, error : action.payload}
         default:
             return state;
     }
