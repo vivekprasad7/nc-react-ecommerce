d3479c4c74a28d352333cd6e8bf5f37692a9fb20
Vivek Prasad <flashtech007@gmail.com>
2023-06-04 06:20:07 +0530

    added user details component

--- src/pages/userAccount/details/details.js
+++ src/pages/userAccount/details/details.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useAuthContext } from '../../../contexts/authContext'
+import "./details.css"
 
 export const Details = () => {
+
+  const {authState : {user}, logoutHandler} = useAuthContext();
+
+  console.log("details",user);
+
+
   return (
-    <div>Details</div>
+    <div className='details-container'>
+      <div className='user-details'>
+          <p>Name: {user.firstName + " " + user.lastName}</p>
+          <p></p>
+          <p>Email : {user.email}</p>
+      </div>
+      <button onClick={logoutHandler}>Logout</button>
+
+    </div>
   )
 }
